45f6fbc2135376ddb3300e9bd3cf13bfc8fa3ec1
fethan <fethan.us@gmail.com>
2021-09-18 18:03:06 +0300

    details mockup created

---
+++ src/components/CoffeeList.js
@@ -0,0 +1,15 @@
+import Title from './Title';
+import Description from './Description';
+import Integridiends from './Integridiends';
+
+const CoffeeList = () => {
+    return (
+        <div className="coffee-case">
+            <Title />
+            <Description />
+            <Integridiends />
+        </div>
+    );
+}
+
+export default CoffeeList;
